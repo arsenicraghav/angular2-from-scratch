c9444ff1eb1556dacb015f2fd1499a500472e3dc
arsenicraghav <mail.raghvendra.kumar@gmail.com>
2016-06-08 03:14:33 +0530

    applied indentation on all files

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,10 +10,10 @@ export class Hero {
   template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
 })
 
-export class AppComponent { 
-   title = 'Tour of Heroes';
-   hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-     };
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
